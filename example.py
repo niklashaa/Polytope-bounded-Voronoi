d2a7d4a4379fdf0e73ac5151f598547c1e168b60
Niklas Haag <niklas.haag@relexsolutions.de>
2020-01-25 15:51:49 +0100

    Fix wCenters & Improve plotting

--- example.py
+++ example.py
@@ -22,22 +22,30 @@ def gauss_heights(areas,heighPar):
     return heighPar*(areas/np.mean(areas)-1)
 
 def plot_voronoi(cells, seeds,centroids):
+    plt.cla()
     ax = plt.axes(projection='3d')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    ax.view_init(elev=90, azim=-90)
+
     ax.plot(seeds[:,0],seeds[:,1],'o')
     ax.plot(centroids[:,0],centroids[:,1],'o')
-
     for cell in cells:
         if len(cell) >= 3:
             hull = ConvexHull(cell)		
             for simplex in hull.simplices:
                 ax.plot(cell[simplex, 0],cell[simplex,1],'k-')
-
     ax.plot_surface(X, Y, phi, cmap='viridis', edgecolor='none', alpha=0.6)
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
-    ax.view_init(elev=90, azim=-90)
-    plt.show()
+    plt.draw()
+    plt.pause(0.01)
+
+def init_phi(X, Y, centers, heights, sigma):
+    phi = np.ones((X.shape[0],X.shape[0]))
+    for i, center in enumerate(centers):
+        gau = heights[i]*np.exp((-((Y-center[1])**2/2)-((X-center[0])**2/2))/sigma)
+        phi += gau
+    return phi
 
 # unweighted centroid
 def uCentroid(cell):
@@ -52,13 +60,6 @@ def uCentroids(cells):
         centroids.append(uCentroid(cell))
     return np.asarray(centroids)
 
-def init_phi(X, Y, centers, heights, sigma):
-    phi = np.ones((X.shape[0],X.shape[0]))
-    for i, center in enumerate(centers):
-        gau = heights[i]*np.exp((-((Y-center[1])**2/2)-((X-center[0])**2/2))/sigma)
-        phi += gau
-    return phi
-
 # weighted centroid
 def wCentroid(cell):
     centroid = []
@@ -71,13 +72,14 @@ def wCentroid(cell):
     reflags = np.reshape(flags, X.shape) # reshaped flags
     
     # With X,Y,flags and phi calculate the weighted centroids:
-    # (X.*flags.*phi)/sum(X.*flags) = centroid(0,0)
-    # (Y.*flags.*phi)/sum(Y.*flags) = centroid(0,1)
+    # (X.*flags.*phi)/sum(phi*flags) = centroid(0,0)
+    # (Y.*flags.*phi)/sum(phi*flags) = centroid(0,1)
     xin = np.multiply(X,reflags)
     yin = np.multiply(Y,reflags)
+    pin = np.multiply(phi,reflags)
 
-    centroid.append(np.sum(np.multiply(xin,phi))/np.sum(xin))
-    centroid.append(np.sum(np.multiply(yin,phi))/np.sum(yin))
+    centroid.append(np.sum(np.multiply(xin,phi))/np.sum(pin))
+    centroid.append(np.sum(np.multiply(yin,phi))/np.sum(pin))
 
     return np.asarray(centroid)
 
@@ -87,25 +89,34 @@ def wCentroids(cells):
         centroids.append(wCentroid(cell))
     return np.asarray(centroids)
 
+def allInBnd(seeds):
+    p = path.Path(bnd)
+    return p.contains_points(seeds).all()
 
+# --------------------------------------------------------------------
 
-# parameter setup
-heighPar = 1
+# setup
+gran = 100
+heighPar = 20
 sigPar = 1
-
-# boundary points, initial seed points
 bnd = np.array([[0, 0], [4, 0], [4, 4], [0, 4]])
-seeds = np.array([[1, 1], [4, 4], [3, 2]])
+# seeds = np.array([[1, 1], [4, 4], [3, 2]])
+seeds = 4*np.random.rand(6,2)
+if not allInBnd(seeds):
+    raise Exception('Boundary does not contain all seeds:\n {}'.format(seeds))
 
 # constants
 mean_area = poly_area(bnd)/len(seeds)
 sigma = sigPar/2*sqrt(mean_area/pi)
 
 # build meshgrid with extreme points of boundary
-gran = 100
 x_range = np.linspace(np.amin(bnd[:,0]),np.amax(bnd[:,0]),gran)
 y_range = np.linspace(np.amin(bnd[:,1]),np.amax(bnd[:,1]),gran)
 X, Y = np.meshgrid(x_range,y_range)
+
+# setup figure
+plt.ion()
+plt.show()
 
 # first iteration
 cells = voronoi(seeds,bnd)
@@ -119,20 +130,23 @@ centroids = wCentroids(cells)
 
 i=0
 # Perform LLoyd algorithm
-while (not np.array_equal(seeds,centroids) and i<=5):
+while not np.array_equal(np.round(seeds,4),np.round(centroids,4)):
 
     i+=1
     print(i)
-    print(centroids)
+    print("areas:")
+    print(areas)
 
     # plot the result...
     plot_voronoi(cells, seeds,centroids)
 
     # next iteration
     seeds = centroids
+    if not allInBnd(seeds):
+        raise Exception('Boundary does not contain all seeds:\n {}'.format(seeds))
     cells = voronoi(seeds,bnd)
     areas = poly_areas(cells)
-    centroids = wCentroids(cells)
+    centroids = uCentroids(cells)
 
     # initialize gauss functions
     areas = poly_areas(cells)
