Fix weighted centroid calculation

FindWeightedCentroids.py
@@ -1,34 +1,53 @@
-from functions import allMoveSafeTowards, gauss_heights, init_phi, plot_voronoi, poly_area, poly_areas, uCentroids, wCentroids
+from functions import allMoveSafeTowards, gauss_heights, sumDist, init_phi, plot_voronoi, poly_area, poly_areas, uCentroids, wCentroids
 from voronoi import voronoi
 
 from sys import maxsize
 import numpy as np
 
 def findWeightedCentroids(seeds, stepsize, sigma, heighpar, bnd, X, Y):
+    aimax = 40
+    ai = aimax
+    stepsize = 0.9
+    decFactor = 0.5
 
     print("Start finding weighted centroids")
     stdevs = [maxsize]
+    sumdists = [maxsize]
     centroids = seeds
+    minseeds = seeds
 
     while True:
+        print(ai)
 
         seeds = allMoveSafeTowards(seeds, centroids, stepsize, bnd)
         cells = voronoi(seeds,bnd)
-        for cell in cells:
-            if cell.shape[1] != 2:
-                print(cell)
         areas = poly_areas(cells)
+        stdev = np.round(np.std(areas),4)
         heights = gauss_heights(areas, heighpar)
         phi = init_phi(X, Y, seeds, heights, sigma)
         centroids = wCentroids(cells, phi, X, Y)
+        sumdist = sumDist(seeds,centroids)
 
         # plot the result
-        # plot_voronoi(cells, seeds, centroids, X, Y, phi)
+        plot_voronoi(cells, seeds, centroids, X, Y, phi)
 
-        stdev = np.round(np.std(areas),4)
+        if sumdist >= min(sumdists):
+            ai -= 1 
+        else:
+            ai = aimax
+            minseeds = seeds
+        if ai == 0:
+            seeds = minseeds
+            centroids = minseeds
+            stdevs.append(min(stdevs))
+            sumdists.append(min(sumdists))
+            stepsize = decFactor*stepsize
+            ai = aimax
+            continue
+
         if np.array_equal(np.round(seeds,3),np.round(centroids,3)):
             break
-        if len(stdevs) > 6 and len(set(stdevs[-6:])) < 3:
-            break
+
         stdevs.append(stdev)
+        sumdists.append(sumdist)
     return stdevs[1:]

config.py
@@ -4,25 +4,20 @@ from matplotlib import path
 from functions import poly_area
 from sys import maxsize
 
-# -----------------------------------------------------------------------------------
-# Setup
-# -----------------------------------------------------------------------------------
-
 bnd = np.array([[0, 0], [9, 0], [9, 9], [0, 9]])
 gran = 200
-heighpar = 100
+heighpar = 10
 seednum = 5
 stepsize = 1
 sigma = 40/2*sqrt(poly_area(bnd)/seednum/pi)
 
-seeds = [9*np.random.rand(2,) for num in range(seednum)]
+#seeds = [9*np.random.rand(2,) for num in range(seednum)]
+seeds = [np.array([0.93563851, 7.85080354]),
+ np.array([0.89696547, 5.60573809]),
+ np.array([7.68339449, 4.31291054]),
+ np.array([5.67128713, 1.26333068]),
+ np.array([3.34779595, 7.91012589])]
 centroids = seeds
 x_range = np.linspace(np.amin(bnd[:,0]),np.amax(bnd[:,0]),gran)
 y_range = np.linspace(np.amin(bnd[:,1]),np.amax(bnd[:,1]),gran)
 X, Y = np.meshgrid(x_range,y_range)
-
-# -----------------------------------------------------------------------------------
-# Data
-# -----------------------------------------------------------------------------------
-
-stdevs = [maxsize]

functions.py
@@ -18,6 +18,12 @@ def allInsideCell(seeds, cells):
         inside *= insideCell(seed, cell)
     return inside
 
+def sumDist(seeds, centroids):
+    dist = 0
+    for seed, centroid in zip(seeds,centroids): 
+        dist += sqrt((centroid[0]-seed[0])**2 + (centroid[1]-seed[1])**2)
+    return dist
+
 # Calculate the area of a polygon
 def poly_area(polytope):
     x = polytope[:,0]
@@ -121,12 +127,12 @@ def init_phi(X, Y, centers, heights, sigma):
     for i, center in enumerate(centers):
         gau = heights[i]*np.exp((-((Y-center[1])**2/2)-((X-center[0])**2/2))/sigma)
         phi += gau
+    phi = phi+np.abs(np.min(phi))
     return phi
 
 # Plot weighted voronoi
 def plot_voronoi(cells, seeds, centroids, X, Y, phi):
     if not plt.fignum_exists(1):
-        plt.ion()
         plt.show()
     ax = plt.axes(projection='3d')
     ax.set_xlabel('x')
@@ -142,5 +148,6 @@ def plot_voronoi(cells, seeds, centroids, X, Y, phi):
             for simplex in hull.simplices:
                 ax.plot(cell[simplex, 0],cell[simplex,1],'k-')
     ax.plot_surface(X, Y, phi, cmap='viridis', edgecolor='none', alpha=0.6)
+#    plt.show() # Use instead of draw and pause to investigate the plot
     plt.draw()
     plt.pause(0.001)